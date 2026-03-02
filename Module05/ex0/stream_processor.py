7548be7a8fcb365d223d33653410c7d5cb8025e7
Sohaib Ahrich <soahrich@c2r5p1.1337.ma>
2026-03-02 15:47:42 +0000

    add some code in ex03

---
+++ Module05/ex0/stream_processor.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+class DataProcessor(ABC):
+
+    @abstractmethod
+    def process(self, data: Any) -> str:
+        pass
+
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        pass
+
+    def format_output(self, result: str) -> str:
+        return f"Output: {result}"
+
+
+class NumericProcessor(DataProcessor):
+    def process(self, data):
+
+        daatlen = 0
+        for i in data:
+            daatlen += 1
+
+        dataTotal = 0
+        for i in data:
+            dataTotal += i
+
+        return f"Sum: {dataTotal}, Count: {daatlen}"
+
+
+class TextProcessor(DataProcessor):
+    def process(self, data):
+        return f"Length: {len(data)} characters"
+
+class LogProcessor(DataProcessor):
+    pass
